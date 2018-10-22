6d80bfddc46012ff0d884c4c1628f9a9773fd9a4
verpilc <verpil_c@etna-alternance.net>
2018-10-22 22:16:16 +0200

    [Score] ajout scoreVerif dans la fonction de score

--- app/score/score/Score.controller.ts
+++ app/score/score/Score.controller.ts
@@ -3,6 +3,8 @@ import { ResultHelpers } from '../../helpers/Result.helpers'
 import { Score } from "./Score.model";
 import { ScoreService } from "./Score.service"
 import { Team } from "../../tunnel/team/Team.model";
+import { ScoreVerif } from "../scoreVerif/ScoreVerif.model";
+import { ScoreVerifService } from "../scoreVerif/ScoreVerif.service";
 
 export class ScoreController {
 
@@ -12,8 +14,12 @@ export class ScoreController {
         var scores = JSON.parse(req.body.scores);
         var i = 0;
         var scoreModels = await ScoreService.FindByMatch(parseInt(res.locals.match.id));
-        var scoresResult:Score[] = [];
-        Object.keys(teams).forEach(async function (key) {
+        var scoresResult = JSON.parse("{}");;
+        scoresResult.scores = [];
+        var scoreVerif = new ScoreVerif();
+        scoreVerif.isError = false;
+        scoreVerif.isValid = true;
+        for (const key of Object.keys(teams)) {
             var teamId = parseInt(teams[key]);
             var score = new Score();
             score.match = res.locals.match;
@@ -28,20 +34,36 @@ export class ScoreController {
             try {
                 var scoreTeam = scoreModels.filter(sco => sco.team.id == teamId)
                 var scoreTeamValid = scoreModels.filter(sco => sco.team.id == teamId && sco.profile.filter(profile => profile.id == res.locals.profile.id).length > 0)
-                if (scoreTeamValid.length <= 0 && (parseInt(res.locals.match.sport.nbTeam)) > scoreTeam.length) {
-                    const scoreResult = await ScoreService.Save(score);
-                    scoresResult.push(scoreResult);
+                if (scoreTeamValid.length <= 0) {
+                    if (parseInt(res.locals.match.sport.nbTeam) == (scoreTeam.length + 1)) {
+                        const scoreResult = await ScoreService.Save(score);
+                        scoreVerif.match = res.locals.match;
+                        scoreTeam.forEach(function(element) {
+                            if (element.scored != score.scored) {
+                                scoreVerif.isError = true;
+                                scoreVerif.isValid = false;
+                            }
+                        })
+                        scoresResult.scores.push(scoreResult);
+                    }
+                    else if (parseInt(res.locals.match.sport.nbTeam) > scoreTeam.length) {
+                        const scoreResult = await ScoreService.Save(score);
+                        scoresResult.scores.push(scoreResult);
+                    }
                 }
             } catch (ex) {
-                console.log(ex);
                 errors.score = "error during saving score(s)";
             }
-        });
+        };
         if (errors && Object.keys(errors).length > 0) {
             return res.status(500).json(ResultHelpers.createReturnJson(500, "error", { server: "internal server error" }));
         }
         else {
-            return res.status(201).json(ResultHelpers.createReturnJson(201, "success", { score: JSON.stringify(scoresResult) }));
+            if (scoreVerif.match){
+                var scoreVerifResult = await ScoreVerifService.Save(scoreVerif);
+                scoresResult.scoreVerif = scoreVerifResult;
+            }
+            return res.status(201).json(ResultHelpers.createReturnJson(201, "success", scoresResult));
         }
     }
 }
