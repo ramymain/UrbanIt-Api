7b172b270a61796099e4972334036dc5b1f278cc
Ramy FERJANI <ramy@ferjani.fr>
2018-10-31 09:40:02 +0100

    heroku

--- config.ts
+++ config.ts
@@ -2,13 +2,29 @@ import { env } from "process";
 
 export const DIALECT = "mysql";
 
+// const LOCAL_CONFIGURATION = {
+// 	DB: "urban_it",
+// 	PASSWORD: "8Md3rp6ty",
+// 	PORT_DB: 3306,
+// 	SERVER: "127.0.0.1",
+// 	USER_DB: "bostonrootuser",
+// };
+
 const LOCAL_CONFIGURATION = {
-	DB: "urban_it",
+	DB: "urbanit",
 	PASSWORD: "8Md3rp6ty",
 	PORT_DB: 3306,
-	SERVER: "127.0.0.1",
+	SERVER: "urbanit.cxxxamtikht7.eu-west-3.rds.amazonaws.com",
 	USER_DB: "bostonrootuser",
 };
+
+// const LOCAL_CONFIGURATION = {
+// 	DB: env.DB || "prod",
+// 	PASSWORD: env.PASSWORD || "",
+// 	PORT_DB: Number(env.PORT_DB) || 3306,
+// 	SERVER: env.SERVER || "localhost",
+// 	USER_DB: env.USER_DB || "root",
+// };
 
 const PRODUCTION_CONFIGURATION = {
 	DB: env.DB || "prod",
