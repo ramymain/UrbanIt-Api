51160e90ea720e9a28f0b1a8b7527060aad0a81a
verpilc <verpil_c@etna-alternance.net>
2018-10-31 14:56:31 +0100

    [Config] git ignore

--- config.ts
+++ config.ts
@@ -3,11 +3,11 @@ import { env } from "process";
 export const DIALECT = "mysql";
 
 const LOCAL_CONFIGURATION = {
-	DB: env.DB || "prod",
-	PASSWORD: env.PASSWORD || "",
-	PORT_DB: Number(env.PORT_DB) || 3306,
-	SERVER: env.SERVER || "localhost",
-	USER_DB: env.USER_DB || "root",
+	DB: "urban_it",
+	PASSWORD: "8Md3rp6ty",
+	PORT_DB: 3306,
+	SERVER: "127.0.0.1",
+	USER_DB: "bostonrootuser",
 };
 
 const PRODUCTION_CONFIGURATION = {
