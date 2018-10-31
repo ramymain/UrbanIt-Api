c4ee9200012e56c9d8551763719c9a26b74dd4d3
verpilc <verpil_c@etna-alternance.net>
2018-10-31 15:26:21 +0100

    [Config] config with env

--- config.ts
+++ config.ts
@@ -3,11 +3,11 @@ import { env } from "process";
 export const DIALECT = "mysql";
 
 const LOCAL_CONFIGURATION = {
-	DB: "urban_it",
-	PASSWORD: "8Md3rp6ty",
-	PORT_DB: 3306,
-	SERVER: "127.0.0.1",
-	USER_DB: "bostonrootuser",
+	DB: env.DB || "prod",
+	PASSWORD: env.PASSWORD || "",
+	PORT_DB: Number(env.PORT_DB) || 3306,
+	SERVER: env.SERVER || "localhost",
+	USER_DB: env.USER_DB || "root",
 };
 
 const PRODUCTION_CONFIGURATION = {
