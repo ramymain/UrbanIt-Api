95f52d905982c90d362becc0e37a35ec8b7dcc02
verpilc <verpil_c@etna-alternance.net>
2018-11-01 21:08:37 +0100

    [User] add profile with all values and relations

--- app/account/user/User.repository.ts
+++ app/account/user/User.repository.ts
@@ -14,22 +14,22 @@ export class UserRepository extends Repository<User> {
     }
 
     public findByText(text: string): Promise<User[]> {
-        return this.manager.find(User, {where: {text}, relations: ["profiles", "profiles.teams", "profiles.sport"]});
+        return this.manager.find(User, {where: {text}, relations: ["profiles", "profiles.teams", "profiles.sport", "profiles.teams.teamLeader", "profiles.teams.teamLeader.profile", "profiles.teams.profiles", "profiles.teams.match", "profiles.teams.match.teams", "profiles.teams.match.teams.profiles", "profiles.teams.match.teams.profiles.user", "profiles.teams.match.teams.teamLeader", "profiles.teams.match.teams.teamLeader.profile"]});
     }
 
     public findOneById(id: number): Promise<User> {
-        return this.manager.findOne(User, {where: {id: id}, relations: ["profiles", "profiles.teams", "profiles.sport"]});
+        return this.manager.findOne(User, {where: {id: id}, relations: ["profiles", "profiles.teams", "profiles.sport", "profiles.teams.teamLeader", "profiles.teams.teamLeader.profile", "profiles.teams.profiles", "profiles.teams.match", "profiles.teams.match.teams", "profiles.teams.match.teams.profiles", "profiles.teams.match.teams.profiles.user", "profiles.teams.match.teams.teamLeader", "profiles.teams.match.teams.teamLeader.profile"]});
     }
 
     public findOneByEmail(email: string): Promise<User> {
-        return this.manager.findOne(User, {where: {email: email}, relations: ["profiles", "profiles.teams", "profiles.sport"]});
+        return this.manager.findOne(User, {where: {email: email}, relations: ["profiles", "profiles.teams", "profiles.sport", "profiles.teams.teamLeader", "profiles.teams.teamLeader.profile", "profiles.teams.profiles", "profiles.teams.match", "profiles.teams.match.teams", "profiles.teams.match.teams.profiles", "profiles.teams.match.teams.profiles.user", "profiles.teams.match.teams.teamLeader", "profiles.teams.match.teams.teamLeader.profile"]});
     }
 
     public findOneByUsername(username: string): Promise<User> {
-        return this.manager.findOne(User, {where: {username: username}, relations: ["profiles", "profiles.teams", "profiles.sport"]});
+        return this.manager.findOne(User, {where: {username: username}, relations: ["profiles", "profiles.teams", "profiles.sport", "profiles.teams.teamLeader", "profiles.teams.teamLeader.profile", "profiles.teams.profiles", "profiles.teams.match", "profiles.teams.match.teams", "profiles.teams.match.teams.profiles", "profiles.teams.match.teams.profiles.user", "profiles.teams.match.teams.teamLeader", "profiles.teams.match.teams.teamLeader.profile"]});
     }
 
     public find(): Promise<User[]> {
-        return this.manager.find(User, {relations: ["profiles", "profiles.teams", "profiles.sport"]});
+        return this.manager.find(User, {relations: ["profiles", "profiles.teams", "profiles.sport", "profiles.teams.teamLeader", "profiles.teams.teamLeader.profile", "profiles.teams.profiles", "profiles.teams.match", "profiles.teams.match.teams", "profiles.teams.match.teams.profiles", "profiles.teams.match.teams.profiles.user", "profiles.teams.match.teams.teamLeader", "profiles.teams.match.teams.teamLeader.profile"]});
     }
 }
